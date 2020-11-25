f1b269be7e433600b9a2f2a0922af218193e9479
Patricio Cabo <59758146+patocabo@users.noreply.github.com>
2020-11-25 12:20:23 -0300

    Added increment decrement button and price

--- app/src/main/java/com/example/justjavav2/MainActivity.java
+++ app/src/main/java/com/example/justjavav2/MainActivity.java
@@ -7,7 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +21,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view){
-        display(1);
+        //int numberOfCoffees = 2;
+        //display(numberOfCoffees);
+        displayPrice(quantity*2);
     }
 
     public void display (int number){
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
         quantityTextView.setText(""+number);
+    }
+
+    public void displayPrice (int number){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment (View view){
+        quantity+=1;
+        display(quantity);
+    }
+    public void decrement (View view){
+        if(quantity>0){
+            quantity-=1;}
+        display(quantity);
     }
 }
